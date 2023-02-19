ba33d1a555b23006a66a16a07aebdc35b1eff654
JamesSenva <jamessenva@gmail.com>
2023-02-19 23:13:35 +0530

    Update coding challenge 1 with if/else statement

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -41,22 +41,22 @@ language = 'Hindi';
 /*  lecture 14
     basic operators
     1. If your country split in half, and each half would contain half the population, then how many people would live in each half? */ 
-        let halfPopulation = population / 2;
-        console.log(halfPopulation);
+        // let halfPopulation = population / 2;
+        // console.log(halfPopulation);
         
 /*   2. Increase the population of your country by 1 and log the result to the console */
-        population++;
-        console.log(population);
+        // population++;
+        // console.log(population);
 
 /*    3. Finland has a population of 6 million. Does your country have more people than Finland? */
-        console.log(population - 6);
+        // console.log(population - 6);
 
 /*   4. The average population of a country is 33 million people. Does your country have less people than the average country? */
-        let averagePopulation = 33;
-        console.log(population < averagePopulation);
+        // let averagePopulation = 33;
+        // console.log(population < averagePopulation);
 
 
-        
+
 /*  5. Based on the variables you created, create a new variable 'description'
     which contains a string with this format: 'Portugal is in Europe, and its 11 million people speak portuguese' */
     
@@ -66,8 +66,30 @@ language = 'Hindi';
 using the template literal syntax
 */
 
-        let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
-        console.log(description);
+        // let description = `${country} is in ${continent}, and its ${population} million people speak ${language}`;
+        // console.log(description);
+
+
+//---------------------------------------------------------------//
+
+/*      lecture 18
+        LECTURE: Taking Decisions: if / else Statements
+
+        1. If your country's population is greater that 33 million, log a string like this to the
+        console: 'Portugal's population is above average'. Otherwise, log a string like
+        'Portugal's population is 22 million below average' (the 22 is the average of 33
+        minus the country's population)
+        2. After checking the result, change the population temporarily to 13 and then to
+        130. See the different results, and set the population back to original
+ */
+
+        // population = 130;
+
+        if (population > 33) {
+                console.log(`${country}'s population is above average.`);
+        } else {
+                console.log(`${country}'s population is ${population - 33} below average`);
+        }
 
 
 //---------------------------------------------------------------//
