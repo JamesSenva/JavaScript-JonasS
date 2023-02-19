42e9e7a110176fe21a7589ed319d6d6115f413a0
JamesSenva <jamessenva@gmail.com>
2023-02-19 17:33:52 +0530

    First commit

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,14 @@
+// console.log('hi');
+
+const country = 'India';
+const continent = 'Asia';
+let population = 1415060319;
+const isIsland = false;
+let language;
+
+console.log(country, typeof country);
+console.log(continent, typeof country);
+console.log(population, typeof population);
+console.log(isIsland, typeof isIsland );
+console.log(language, typeof language );
+console.log(typeof null);
---
+++ 01-Fundamentals-Part-1/starter/main.js
@@ -0,0 +1,8 @@
+// console.log('hello');
+// console.log(40 + 8 + 23 - 10);
+
+// let js = 'amazing';
+// if(js === 'amazing'){
+//     console.log('fun');
+// }
+
