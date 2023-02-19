5f52bde8a1c94d4555918e398f3feee3e87f8446
JamesSenva <jamessenva@gmail.com>
2023-02-19 17:48:10 +0530

    Use let and const variables

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -6,6 +6,28 @@ let population = 1415060319;
 const isIsland = false;
 let language;
 
+// console.log(country, typeof country);
+// console.log(continent, typeof country);
+// console.log(population, typeof population);
+// console.log(isIsland, typeof isIsland );
+// console.log(language, typeof language );
+// console.log(typeof null);
+
+//---------------------------------------------------------------
+
+// lecture 13
+// let, const and var
+
+/* 
+    1. Set the value of 'language' to the language spoken where you live (some
+    countries have multiple languages, but just choose one)
+    2. Think about which variables should be const variables (which values will never
+    change, and which might change?). Then, change these variables to const.
+    3. Try to change one of the changed variables now, and observe what happens
+*/
+
+language = 'Hindi';
+
 console.log(country, typeof country);
 console.log(continent, typeof country);
 console.log(population, typeof population);
