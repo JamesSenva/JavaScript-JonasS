d10163be280c675b1a05b93392e5b2da1318cf43
JamesSenva <jamessenva@gmail.com>
2023-02-20 22:36:28 +0530

    Guess type conversion and coercion

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -94,3 +94,14 @@ using the template literal syntax
 
 //---------------------------------------------------------------//
 
+/*      lecture 20
+        LECTURE: Type Conversion and Coercion
+1. Predict the result of these 5 operations without executing them: */
+        console.log('9' - '5'); // 4
+        console.log('19' - '13' + '17'); // 617
+        console.log('19' - '13' + 17); // 23
+        console.log('123' < 57);     // false
+        console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143
+/* 2. Execute the operations to check if you were right */
+
+//---------------------------------------------------------------//
