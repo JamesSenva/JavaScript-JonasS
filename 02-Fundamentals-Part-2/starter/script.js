5b0862abb069f72546c8b40b498470b57839f75e
JamesSenva <jamessenva@gmail.com>
2023-02-28 22:55:31 +0530

    Call function withing other function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -57,9 +57,34 @@
 //     return ((100 * population) / 7900).toFixed(1);
 // }
 
-const percentageOfWorld3 = population => (100 * population) / 7900;
+// const percentageOfWorld3 = population => (100 * population) / 7900;
 
-console.log(percentageOfWorld3(1394));
+// console.log(percentageOfWorld3(1394));
 
 //------------------------------------------------------------------
 
+// lecture 36 - functions calling other functions
+
+// 1. Create a function called 'describePopulation'. Use the function type you like the most. This function takes in two arguments: 'country' and 'population', and returns a string like this: 'China has 1441 million people which is about 18.2% of the world.'
+
+const percentageOfWorld3 = population => ((100 * population) / 7900).toFixed(1);
+
+// const describePopulation = (country, population) => {
+//     return `${country} has ${population} million people which is about ${percentageOfWorld3(population)} % of the world.`
+// }
+
+const describePopulation = (country, population) => {
+    const percentage = percentageOfWorld3(population);
+    const description = `${country} has ${population} million people which is about ${percentage}% of the world.`
+    console.log(description);
+}
+// 2. To calculate the percentage, 'describePopulation' call the 'percentageOfWorld1' you created earlier
+
+// 3. Call 'describePopulation' with data for 3 countries of your choice
+
+describePopulation('China', 1441);
+describePopulation('India', 1394);
+describePopulation('USA', 500);
+
+
+//------------------------------------------------------------------
