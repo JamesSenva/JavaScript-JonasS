f55db88547f34c022cf8b6fc726ab59f15095cd3
JamesSenva <jamessenva@gmail.com>
2023-02-28 22:22:08 +0530

    Use arrow function

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -27,24 +27,39 @@
 
 // 1. The world population is 7900 million people. Create a function declaration called 'percentageOfWorld1' which receives a 'population' value, and returns the percentage of the world population that the given population represents. For example, China has 1441 million people, so it's about 18.2% of the world population
 
-function percentageOfWorld1 (population) {
-    return ((100 * population) / 7900).toFixed(1);
-}
+// function percentageOfWorld1 (population) {
+//     return ((100 * population) / 7900).toFixed(1);
+// }
 
 // console.log(percentageOfWorld1(1394).toFixed(1));
 
-const populationIndia = percentageOfWorld1(1394);
-const populationChina = percentageOfWorld1(1441);
-const populationUSA = percentageOfWorld1(500);
+// const populationIndia = percentageOfWorld1(1394);
+// const populationChina = percentageOfWorld1(1441);
+// const populationUSA = percentageOfWorld1(500);
 
-console.log(populationChina, populationIndia, populationUSA);
+// console.log(populationChina, populationIndia, populationUSA);
 
-const percentageOfWorld2 = function(population){
-    return ((100 * population) / 7900).toFixed(1);
-}
+// const percentageOfWorld2 = function(population){
+//     return ((100 * population) / 7900).toFixed(1);
+// }
 
-console.log(percentageOfWorld2(1394));
-console.log(percentageOfWorld2(1441));
-console.log(percentageOfWorld2(500));
+// console.log(percentageOfWorld2(1394));
+// console.log(percentageOfWorld2(1441));
+// console.log(percentageOfWorld2(500));
 
 //------------------------------------------------------------------
+
+// lecture 35 - arrow functions
+
+// 1. Recreate the last assignment, but this time create an arrow function called 'percentageOfWorld3'
+
+// const percentageOfWorld3 = (population) => {
+//     return ((100 * population) / 7900).toFixed(1);
+// }
+
+const percentageOfWorld3 = population => (100 * population) / 7900;
+
+console.log(percentageOfWorld3(1394));
+
+//------------------------------------------------------------------
+
