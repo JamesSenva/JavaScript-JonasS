ff98d578268e21e31517c1b7f28de52f024dfe89
JamesSenva <jamessenva@gmail.com>
2023-03-02 23:06:59 +0530

    Use array with functions to calculate tips

--- 02-Fundamentals-Part-2/starter/coding_challenges.js
+++ 02-Fundamentals-Part-2/starter/coding_challenges.js
@@ -29,25 +29,89 @@ Hints:
 Apply this to the team's average scores 😉
 GOOD LUCK 😀 */
 
-const calcAverage = (score1, score2, score3) => {
-    return (score1 + score2 + score3) / 3;
-};
+    // const calcAverage = (score1, score2, score3) => {
+    //     return (score1 + score2 + score3) / 3;
+    // };
 
-// const avgDolphins = calcAverage(44, 23, 71);
-// const avgKoalas = calcAverage(65, 54, 49);
+    // const avgDolphins = calcAverage(44, 23, 71);
+    // const avgKoalas = calcAverage(65, 54, 49);
 
-const avgDolphins = calcAverage(85, 54, 41);
-const avgKoalas = calcAverage(23, 34, 27);
+    // const avgDolphins = calcAverage(85, 54, 41);
+    // const avgKoalas = calcAverage(23, 34, 27);
 
-const checkWinner = (avgDolphins, avgKoalas) => {
-    if(avgDolphins >= 2 * avgKoalas){
-        console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
-    }else if (avgKoalas >= 2 * avgDolphins){
-        console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
-    }else{
-        console.log('No team wins!');
-    }
-}
-checkWinner(avgDolphins, avgKoalas);
+    // const checkWinner = (avgDolphins, avgKoalas) => {
+    //     if(avgDolphins >= 2 * avgKoalas){
+    //         console.log(`Dolphins win (${avgDolphins} vs ${avgKoalas})`)
+    //     }else if (avgKoalas >= 2 * avgDolphins){
+    //         console.log(`Koalas win (${avgKoalas} vs ${avgDolphins})`)
+    //     }else{
+    //         console.log('No team wins!');
+    //     }
+    // }
+    // checkWinner(avgDolphins, avgKoalas);
 
 //---------------------------------------------------------------------
+
+/* 
+Coding Challenge #2
+Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+20%.
+Your tasks:
+1. Write a function 'calcTip' that takes any bill value as an input and returns
+the corresponding tip, calculated based on the rules above (you can check out
+the code from first tip calculator challenge if you need to). Use the function
+type you like the most. Test the function using a bill value of 100
+2. And now let's use arrays! So create an array 'bills' containing the test data
+below
+3. Create an array 'tips' containing the tip value for each bill, calculated from
+the function you created before
+4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+Test data: 125, 555 and 44
+Hint: Remember that an array needs a value in each position, and that value can
+actually be the returned value of a function! So you can just call a function as array
+values (so don't store the tip values in separate variables first, but right in the new
+array) 😉
+GOOD LUCK 😀  */
+
+    const calcTip = (bill) => {
+        if(bill >= 50 && bill <= 300){
+            return ((bill * 15) / 100 );
+        }else{
+            return ((bill * 20) / 100);
+        }
+    };
+
+    // calcTip(400);
+
+    const bills = [125, 555, 44];
+
+    const tips = [];
+
+    bills.forEach(bill => {
+        return tips.push(calcTip(bill));
+    })
+
+    console.log(tips);
+
+    const total = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+
+    // const total = [];
+    
+    // for( let i = 0; i <= bills.length - 1; i++){
+    //     // console.log(bills[i]);
+    //     // tips.forEach( tip => {
+    //     //     const totalSum =  tip + bills[i];
+    //     //     total.push(totalSum);
+    //     // })
+    //     for(let j = 0; j <= tips.length - 1; j++){
+    //         const totalSum = tips[i] + bills[i];
+    //         total.push(totalSum)
+    //     }
+    // }
+    // const calcTotal = (billSum, tipSum) => {
+    //     return total.push(billSum + tipSum);
+    // }
+
+    // total.push(calcTotal(bills[0], tips[0]));
+    console.log('total:', total);
