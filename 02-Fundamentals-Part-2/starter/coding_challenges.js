ede57f797b5b3a197e53587ac7de5533986f2cb9
JamesSenva <jamessenva@gmail.com>
2023-03-05 20:19:33 +0530

    Use for and while loop to iterate over an array

--- 02-Fundamentals-Part-2/starter/coding_challenges.js
+++ 02-Fundamentals-Part-2/starter/coding_challenges.js
@@ -201,36 +201,115 @@ from the method
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
 GOOD LUCK 😀 */ 
 
-    const markInfo = {
-        firstName: 'Mark',
-        lastName: 'Miller',
-        weight: 78,
-        height: 1.69,
-        calcBMI: function(){
-            this.BMI = (this.weight / this.height ** 2).toFixed(1);
-            return this.BMI;
+    // const markInfo = {
+    //     firstName: 'Mark',
+    //     lastName: 'Miller',
+    //     weight: 78,
+    //     height: 1.69,
+    //     calcBMI: function(){
+    //         this.BMI = (this.weight / this.height ** 2).toFixed(1);
+    //         return this.BMI;
+    //     }
+    // }
+
+    // markInfo.calcBMI();
+    // // console.log(markInfo.BMI);
+
+    // const johnInfo = {
+    //     firstName: 'John',
+    //     lastName: 'Smith',
+    //     weight: 92,
+    //     height: 1.95,
+    //     calcBMI: function(){
+    //         this.BMI = (this.weight / this.height ** 2).toFixed(1);
+    //         return this.BMI;
+    //     }
+    // }
+
+    // johnInfo.calcBMI();
+
+
+    // if(markInfo.BMI > johnInfo.BMI){
+    //     console.log(`${markInfo.firstName} ${markInfo.lastName}'s BMI (${markInfo.BMI}) is higher than ${johnInfo.firstName} ${johnInfo.lastName} BMI (${johnInfo.BMI})`);
+    // }else{
+    //     console.log(`${johnInfo.firstName} ${johnInfo.lastName}'s BMI (${johnInfo.BMI}) is higher than ${markInfo.firstName} ${markInfo.lastName}'s BMI (${markInfo.BMI})`);
+    // }
+
+//---------------------------------------------------------------------
+
+/*
+
+Coding Challenge #4
+Let's improve Steven's tip calculator even more, this time using loops!
+
+Your tasks:
+1. Create an array 'bills' containing all 10 test bill values
+2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+tips and totals arrays 😉
+
+Bonus:
+4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+an argument. This function calculates the average of all numbers in the given
+array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+
+4.1. First, you will need to add up all values in the array. To do the addition,
+start by creating a variable 'sum' that starts at 0. Then loop over the
+array using a for loop. In each iteration, add the current value to the
+'sum' variable. This way, by the end of the loop, you have all values
+added together
+
+4.2. To calculate the average, divide the sum you calculated before by the
+length of the array (because that's the number of elements)
+
+4.3. Call the function with the 'totals' array
+
+GOOD LUCK 😀 */ 
+
+    const calcTip = (bill) => {
+        if(bill >= 50 && bill <= 300){
+            return ((bill * 15) / 100 );
+        }else{
+            return ((bill * 20) / 100);
         }
+    };
+
+    const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+    const tips = [];
+    const totals = [];
+
+    for(let i = 0; i < bills.length; i++){
+        let tip = calcTip(bills[i]);
+        tips.push(tip);
+        
+        let total = bills[i] + tip;
+        totals.push(total);
     }
 
-    markInfo.calcBMI();
-    // console.log(markInfo.BMI);
+    console.log('Bills: ', bills);
+    console.log('Tips: ', tips);
+    console.log('Totals: ', totals);
 
-    const johnInfo = {
-        firstName: 'John',
-        lastName: 'Smith',
-        weight: 92,
-        height: 1.95,
-        calcBMI: function(){
-            this.BMI = (this.weight / this.height ** 2).toFixed(1);
-            return this.BMI;
+    const calcAverage = (arr) => {
+        let sum = 0;
+
+        // for loop
+        // for(let i = 0; i < arr.length; i++){
+        //     sum += arr[i];
+        // }
+
+        // while loop
+        let i = 0;
+        while(i < arr.length){
+            sum += arr[i];
+            i++;
         }
+        let average = sum / arr.length;
+        console.log('while loop:',average);
     }
 
-    johnInfo.calcBMI();
-
-
-    if(markInfo.BMI > johnInfo.BMI){
-        console.log(`${markInfo.firstName} ${markInfo.lastName}'s BMI (${markInfo.BMI}) is higher than ${johnInfo.firstName} ${johnInfo.lastName} BMI (${johnInfo.BMI})`);
-    }else{
-        console.log(`${johnInfo.firstName} ${johnInfo.lastName}'s BMI (${johnInfo.BMI}) is higher than ${markInfo.firstName} ${markInfo.lastName}'s BMI (${markInfo.BMI})`);
-    }
+    calcAverage(totals);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -300,21 +300,21 @@
 
 // 1. Recreate the challenge from the lecture 'Looping Arrays, Breaking and Continuing', but this time using a while loop (call the array 'percentages3')
 
-    const populations = [10, 1441, 332, 83];
+    // const populations = [10, 1441, 332, 83];
 
-    const percentages3 = [];
+    // const percentages3 = [];
 
-    function percentageOfWorld1 (populations) {
-            return ((100 * populations) / 7900).toFixed(1);
-    }
+    // function percentageOfWorld1 (populations) {
+    //         return ((100 * populations) / 7900).toFixed(1);
+    // }
 
-    let i = 0;
-    while(i < populations.length){
-        const perc = percentageOfWorld1(populations[i]);
-        percentages3.push(perc);
-        i++
-    }
+    // let i = 0;
+    // while(i < populations.length){
+    //     const perc = percentageOfWorld1(populations[i]);
+    //     percentages3.push(perc);
+    //     i++
+    // }
 
-    console.log(percentages3);
-    
+    // console.log(percentages3);
+
 // 2. Reflect on what solution you like better for this task: the for loop or the while loop?
