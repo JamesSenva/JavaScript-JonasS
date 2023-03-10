d4b1aa5ce5feb88e6815e1e11516d436d080330e
JamesSenva <jamessenva@gmail.com>
2023-03-10 23:25:07 +0530

    Select the element for the UI of the game check-message-guess classes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const check = document.querySelector('.check');
+const message = document.querySelector('.message');
+const guess = +document.querySelector('.guess');
+
+const number = function() {
+    console.log(guess.value, typeof guess);
+    if(!guess){
+        message.textContent = 'Not a number!';
+    }
+};
+
+check.addEventListener('click', number);
+
+
