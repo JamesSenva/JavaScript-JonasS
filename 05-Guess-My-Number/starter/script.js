ea58c374591509c3f500b4b57cfc8d82b7423f06
JamesSenva <jamessenva@gmail.com>
2023-03-14 22:54:11 +0530

    Build most of the game logic and functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,14 +2,54 @@
 
 const check = document.querySelector('.check');
 const message = document.querySelector('.message');
-const guess = +document.querySelector('.guess');
+const guess = Number(document.querySelector('.guess').value);
+const scoreDOM = document.querySelector('.score');
 
-const number = function() {
-    console.log(guess.value, typeof guess);
-    if(!guess){
-        message.textContent = 'Not a number!';
+// console.log(guess, typeof guess);
+
+// generate secret number to compare with the entered number
+const secretNumber = Math.trunc(Math.random() * 21);
+
+// score
+// if the guess is lower than secret number reduce 1
+// if the guess is greater than secret number add 1
+// should not go beyond 20;
+let score = 20;
+
+// outputs the number in the DOM
+document.querySelector('.number').textContent = secretNumber;
+
+// seperate function for the event listener
+function number(){
+
+    console.log(guess, typeof guess);
+
+    if (!guess) {
+        message.textContent = '⛔ Not a number';
+    } else if (guess === secretNumber) {
+        message.textContent = '🎉 Correct Number!'
+    } else if (guess > secretNumber) {
+        if (score > 1){
+            message.textContent = '📈 Too high';
+            score--;
+            scoreDOM.textContent = score;
+        } else {
+            message.textContent = 'You lost the game 😥';
+            scoreDOM.textContent = 0;
+        }
+    } else if (guess < secretNumber) {
+        if (score > 1){
+            message.textContent = '📉 Too low';
+            score--;
+            scoreDOM.textContent = score;
+        } else {
+            message.textContent = 'You lost the game 😥';
+            scoreDOM.textContent = 0;
+        }
     }
-};
+
+}
+
 
 check.addEventListener('click', number);
 
