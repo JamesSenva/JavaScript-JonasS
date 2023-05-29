28fadde54bca4d79924719db7e21e6199391a675
JamesSenva <jamessenva@gmail.com>
2023-05-29 22:13:27 +0530

    Learn about scope chain

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function calcAge(birthYear) {
+    const age = 2037 - birthYear;
+    // console.log(firstName);
+
+    function printAge() {
+        const output = `${firstName}, You are ${age}, born in ${birthYear}`
+        console.log(output);
+
+        // block scope example
+        if(birthYear >= 1981 && birthYear <= 1996) {
+            var millenial = true;
+            const firstName = 'Steven'
+            const str = `Oh, and you are a millenial, ${firstName}`;
+            console.log(str);
+
+            function add(a, b){
+                return a + b;
+            }
+        }
+        // console.log(str);
+        console.log(millenial); // variable declared with var is accessible outside of block scope
+        // console.log(add(2,3));
+    }
+    printAge();
+    return age;
+}
+
+const firstName = 'James';
+calcAge(1991);
+// printAge();
+// console.log(age);
